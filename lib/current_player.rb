67d2710e8c12ecbffd707d1434210b96475139a0
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-14 20:06:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,18 +2,17 @@
 
 
 
-def turn_count(board) 
+def turn_count(board)
   turns = 0
   board.each do | xoro |
     if xoro == "X" || xoro == "O"
       turns += 1
-    end 
-  end 
-  return turns 
-end 
+    end
+  end
+  return turns
+end
 
 
 def current_player
 
-end 
-
+end
